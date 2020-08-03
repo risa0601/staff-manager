a7f6f2056af799d49f43f626ce42a907bcb5383b
r.a <1307@yahoo.co.jp>
2020-08-03 12:00:08 +0000

    radio_button for schedule_new

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -4,6 +4,15 @@ class ShiftsController < ApplicationController
   end
 
   def create
+    @shift = Shift.new(shift_params)
+    
+    if @shift.save
+      flash[:success] = "シフト希望を提出しました"
+      redirect_to shifts_user_path(current_user)
+    else
+      flash[:danger] = "シフト希望を提出できませんでした"
+      render :new
+    end
   end
 
 end
