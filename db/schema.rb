5d19ad5cf6150a5c8006aaec32fc36fcd3126bab
r.a <1307@yahoo.co.jp>
2020-08-22 15:08:17 +0000

    ユーザテーブルにemailカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200818090811) do
+ActiveRecord::Schema.define(version: 20200822145648) do
 
   create_table "having_skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20200818090811) do
     t.string   "phone"
     t.text     "memo",             limit: 65535
     t.text     "position",         limit: 65535
+    t.string   "email",                                          null: false
   end
 
   add_foreign_key "having_skills", "skills"
