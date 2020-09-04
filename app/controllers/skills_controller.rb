98ae2e3bbd79d0ab60ea4be2c9e131677fa67d67
r.a <1307@yahoo.co.jp>
2020-09-04 08:41:15 +0000

    スキルの登録、一覧を作成

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,13 +1,34 @@
 class SkillsController < ApplicationController
   def index
+    @skills = Skill.all
   end
 
   def new
+    @skill = Skill.new
   end
 
   def create
+    @skill = Skill.new(skill_params)
+    
+    if @skill.save
+      flash[:success] = "スキルを登録しました"
+      redirect_to skills_path
+    else
+      flash.now[:danger] = "スキルの登録に失敗しました"
+      render :new
+    end
   end
 
   def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+    flash[:success] = "スキルを削除しました"
+    redirect_to skills_url
+  end
+  
+  private
+  
+  def skill_params
+    params.require(:skill).permit(:skill_name)
   end
 end
