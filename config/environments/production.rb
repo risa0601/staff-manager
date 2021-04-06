f3d2f843c57a3252b71ba9d8941b642ce45f2cb6
risa0601 <76433311+risa0601@users.noreply.github.com>
2021-04-06 22:23:47 +0900

    デプロイ時の追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # デプロイ時に追加
+  config.assets.compile = true
+  config.assets.initialize_on_precompile=false
 end
